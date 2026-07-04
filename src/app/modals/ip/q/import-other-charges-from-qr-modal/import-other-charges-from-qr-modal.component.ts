e933d358a2216e57ac93f53938fd40e31c62779a
Julian David Bayer <jdbayer09@hotmail.com>
2026-07-04 13:09:10 -0500

    Modal de Other Charges completo

--- src/app/modals/ip/q/import-other-charges-from-qr-modal/import-other-charges-from-qr-modal.component.ts
+++ src/app/modals/ip/q/import-other-charges-from-qr-modal/import-other-charges-from-qr-modal.component.ts
@@ -5,6 +5,10 @@ import { UtilService } from '@services/util';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { finalize } from 'rxjs';
 
+interface AvailableOtherChargeDisplay extends IpQuotationOtherChargeAvailableFromQr {
+  selected: boolean;
+}
+
 @Component({
   selector: 'app-import-other-charges-from-qr-modal',
   templateUrl: './import-other-charges-from-qr-modal.component.html',
@@ -34,13 +38,12 @@ export class ImportOtherChargesFromQrModalComponent implements OnInit {
     return map;
   });
 
-  protected _availableCharges = signal<IpQuotationOtherChargeAvailableFromQr[]>([]);
-  availableCharges = computed<IpQuotationOtherChargeAvailableFromQr[]>(() => this._availableCharges());
-
-  protected _selectedIds = signal<Set<string>>(new Set());
+  protected _availableCharges = signal<AvailableOtherChargeDisplay[]>([]);
+  availableCharges = computed<AvailableOtherChargeDisplay[]>(() => this._availableCharges());
 
   protected _importing = signal<boolean>(false);
   importing = computed<boolean>(() => this._importing());
+  hasSelection = computed<boolean>(() => this._availableCharges().some(c => c.selected));
 
   ngOnInit(): void {
     this.loadAvailableCharges();
@@ -52,7 +55,7 @@ export class ImportOtherChargesFromQrModalComponent implements OnInit {
         finalize(() => this._loading.set(false))
       )
       .subscribe({
-        next: (resp) => this._availableCharges.set(resp),
+        next: (resp) => this._availableCharges.set(resp.map(c => ({ ...c, selected: false }))),
         error: (err) => {
           this.utilSV.setMessage('Error', err, 'error');
           this.ref.close({ valid: false });
@@ -61,34 +64,31 @@ export class ImportOtherChargesFromQrModalComponent implements OnInit {
   }
 
   toggleSelect(id: string): void {
-    this._selectedIds.update(ids => {
-      const newIds = new Set(ids);
-      if (newIds.has(id)) {
-        newIds.delete(id);
-      } else {
-        newIds.add(id);
-      }
-      return newIds;
+    this._availableCharges.update(items => {
+      const idx = items.findIndex(i => i.id === id);
+      if (idx === -1) return items;
+      const updated = [...items];
+      updated[idx] = { ...updated[idx], selected: !updated[idx].selected };
+      return updated;
     });
   }
 
   importSelected(): void {
-    if (this._selectedIds().size === 0) return;
+    const selected = this._availableCharges().filter(c => c.selected);
+    if (selected.length === 0) return;
 
     this._importing.set(true);
 
     const qrMap = this.qrNumberToQqrId();
     const items: IpQuotationOtherChargeImportItem[] = [];
 
-    for (const charge of this._availableCharges()) {
-      if (this._selectedIds().has(charge.id)) {
-        const quotationsQuoteRequestId = qrMap.get(charge.qrNumber);
-        if (quotationsQuoteRequestId) {
-          items.push({
-            quotationsQuoteRequestId,
-            qrOtherChargeId: charge.id
-          });
-        }
+    for (const charge of selected) {
+      const quotationsQuoteRequestId = qrMap.get(charge.qrNumber);
+      if (quotationsQuoteRequestId) {
+        items.push({
+          quotationsQuoteRequestId,
+          qrOtherChargeId: charge.id
+        });
       }
     }
 
--- src/app/modals/ip/q/import-other-charges-from-qr-modal/import-other-charges-from-qr-modal.module.ts
+++ src/app/modals/ip/q/import-other-charges-from-qr-modal/import-other-charges-from-qr-modal.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ImportOtherChargesFromQrModalComponent } from './import-other-charges-from-qr-modal.component';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { ProgressBarModule } from 'primeng/progressbar';
+import { CheckboxModule } from 'primeng/checkbox';
 import { PipesModule } from '@pipes/pipes.module';
 
 @NgModule({
@@ -12,9 +14,11 @@ import { PipesModule } from '@pipes/pipes.module';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     ButtonModule,
     TableModule,
     ProgressBarModule,
+    CheckboxModule,
     PipesModule
   ],
   exports: [
