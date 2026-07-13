12dd61a8c274bb70dc58dac6ad230d7863c0b4b8
Julian David Bayer <jdbayer09@hotmail.com>
2026-07-13 16:21:45 -0500

    chore(config): register Purchase_Orders module name and messages

--- src/app/config/types/tabs/modulesNames.type.ts
+++ src/app/config/types/tabs/modulesNames.type.ts
@@ -1,2 +1,2 @@
-export type NameModules = 'Clients' | 'Departments' | 'Roles' | 'Suppliers' | 'Users' | 'Locations' | 'Industries' | 'Dashboard' | 'Docs' | 'Brands' | 'Products' | 'Quote_Requests' | 'Quotations' | '';
-export type Modules = 'user' | 'department' | 'industry' | 'role' | 'client' | 'supplier' | 'brand' | 'products' | 'QR' | 'Q' | '';
+export type NameModules = 'Clients' | 'Departments' | 'Roles' | 'Suppliers' | 'Users' | 'Locations' | 'Industries' | 'Dashboard' | 'Docs' | 'Brands' | 'Products' | 'Quote_Requests' | 'Quotations' | 'Purchase_Orders' | '';
+export type Modules = 'user' | 'department' | 'industry' | 'role' | 'client' | 'supplier' | 'brand' | 'products' | 'QR' | 'Q' | 'PO' | '';
